df9c9a2e779d83058bc96da6ff15e6e03ec4e8b9
Phu Tang <tangphu02@gmail.com>
2018-06-05 22:36:55 +0700

    update cronjobs with main db

--- app1/app/models/seed.rb
+++ app1/app/models/seed.rb
@@ -5,6 +5,10 @@ class Seed < ApplicationRecord
   scope :available, -> { where(consumed: false, app_id: 1) }
   scope :current, -> { where(app_id: 1) }
 
+  def app_id=(aid)
+    write_attribute(:app_id, aid)
+  end
+
   private
   def init_data
     self.label = "1-#{rand}"
--- app2/app/models/seed.rb
+++ app2/app/models/seed.rb
@@ -5,6 +5,10 @@ class Seed < ApplicationRecord
   scope :available, -> { where(consumed: false, app_id: 2) }
   scope :current, -> { where(app_id: 2) }
 
+  def app_id=(aid)
+    write_attribute(:app_id, aid)
+  end
+
   private
   def init_data
     self.label = "2-#{rand}"
